debd28a426db6a2b757411307f34f6ac78bfbecf
James Coglan <jcoglan@googlemail.com>
2008-01-07 19:18:03 +0000

    Restructuring Enumerable so that it won't overwrite each() if the class already has it. Also added some more testing.

--- source/enumerable.js
+++ source/enumerable.js
@@ -1,29 +1,40 @@
-JS.Enumerable = JS.Module({
-  each: function(block, context) {
+JS.Enumerable = (function() {
+  
+  var each = function(block, context) {
     for (var i = 0, n = this.length; i < n; i++)
       block.call(context || null, this[i], i);
-  },
+  };
   
-  inject: function(memo, block, context) {
-    if (typeof memo == 'function') {
-      context = block; block = memo; memo = undefined;
+  var methods = {
+    inject: function(memo, block, context) {
+      if (typeof memo == 'function') {
+        context = block; block = memo; memo = undefined;
+      }
+      var counter = 0;
+      this.each(function(item, i) {
+        if (!counter++ && memo === undefined) return memo = item;
+        memo = block.call(context || null, memo, item, i);
+      });
+      return memo;
+    },
+    
+    all: function(block, context) {
+      return this.inject(true, function(memo, item, i) {
+        return memo && block.call(context || null, item, i);
+      });
+    },
+    
+    any: function(block, context) {
+      return this.inject(false, function(memo, item, i) {
+        return memo || block.call(context || null, item, i);
+      });
     }
-    this.each(function(item, i) {
-      if (i == 0 && memo === undefined) return memo = item;
-      memo = block.call(context || null, memo, item, i);
-    });
-    return memo;
-  },
+  };
   
-  all: function(block, context) {
-    return this.inject(true, function(memo, item, i) {
-      return memo && block.call(context || null, item, i);
-    });
-  },
-  
-  any: function(block, context) {
-    return this.inject(false, function(memo, item, i) {
-      return memo || block.call(context || null, item, i);
-    });
-  }
-});
+  return {
+    included: function(klass) {
+      if (!klass.prototype.each) klass.include({each: each});
+      klass.include(methods);
+    }
+  };
+})();
