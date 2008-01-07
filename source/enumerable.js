0e1fca4b9407bc1a197fca63a02c8fdabc120a48
James Coglan <jcoglan@googlemail.com>
2008-01-07 19:27:19 +0000

    Added Enumerable#map.

--- source/enumerable.js
+++ source/enumerable.js
@@ -1,8 +1,10 @@
 JS.Enumerable = (function() {
   
+  var Null = function(context) { return context || null; };
+  
   var each = function(block, context) {
     for (var i = 0, n = this.length; i < n; i++)
-      block.call(context || null, this[i], i);
+      block.call(Null(context), this[i], i);
   };
   
   var methods = {
@@ -13,23 +15,33 @@ JS.Enumerable = (function() {
       var counter = 0;
       this.each(function(item, i) {
         if (!counter++ && memo === undefined) return memo = item;
-        memo = block.call(context || null, memo, item, i);
+        memo = block.call(Null(context), memo, item, i);
       });
       return memo;
     },
     
     all: function(block, context) {
       return this.inject(true, function(memo, item, i) {
-        return memo && block.call(context || null, item, i);
+        return memo && block.call(Null(context), item, i);
       });
     },
     
     any: function(block, context) {
       return this.inject(false, function(memo, item, i) {
-        return memo || block.call(context || null, item, i);
+        return memo || block.call(Null(context), item, i);
+      });
+    },
+    
+    map: function(block, context) {
+      return this.inject([], function(memo, item, i) {
+        memo.push(block.call(Null(context), item, i));
+        return memo;
       });
     }
   };
+  
+  var alias = {map: 'collect'};
+  for (var key in alias) methods[alias[key]] = methods[key];
   
   return {
     included: function(klass) {
