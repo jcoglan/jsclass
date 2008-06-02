a4e58b03aa708fda202c85fc64510d0ea6c7cf23
James Coglan <jcoglan@googlemail.com>
2008-06-02 02:28:54 +0000

    Moving the method-binding function into ObjectMethods.

--- source/class.js
+++ source/class.js
@@ -33,12 +33,6 @@ JS = {
     return object;
   },
   
-  method: function(name) {
-    var self = this, cache = self.__mcache__ = self.__mcache__ || {};
-    if ((cache[name] || {}).fn == self[name]) return cache[name].bd;
-    return (cache[name] = {fn: self[name], bd: self[name].bind(self)}).bd;
-  },
-  
   makeFunction: function() {
     return function() {
       return this.initialize.apply(this, arguments) || this;
@@ -156,6 +150,12 @@ JS.ObjectMethods = new JS.Module({
   
   isA: function(moduleOrClass) {
     return this.__eigen__().includes(moduleOrClass);
+  },
+  
+  method: function(name) {
+    var self = this, cache = self.__mcache__ = self.__mcache__ || {};
+    if ((cache[name] || {}).fn == self[name]) return cache[name].bd;
+    return (cache[name] = {fn: self[name], bd: self[name].bind(self)}).bd;
   }
 });
 
