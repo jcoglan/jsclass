9fe2e3a9ccc8d96d36a1c6633790c3fc6a06c8f7
James Coglan <jcoglan@googlemail.com>
2008-06-03 20:47:52 +0000

    Updating Proxy.Virtual for modular branch.

--- source/proxy.js
+++ source/proxy.js
@@ -1,51 +1,54 @@
 if (JS.Proxy === undefined) JS.Proxy = {};
 
-JS.Proxy.Virtual = function(klass) {
-  
-  var bridge = function() {}, self = arguments.callee;
-  bridge.prototype = klass.prototype;
-  
-  var proxy = JS.Class(), func;
-  
-  for (var method in klass.prototype) {
-    func = klass.prototype[method];
-    if (Function.is(func) && func != klass) func = self.forward(method);
-    proxy.instanceMethod(method, func);
-  }
-  
-  proxy.include({
-    initialize: function() {
-      var args = arguments, subject = null;
-      this._getSubject = function() {
-        subject = new bridge;
-        klass.apply(subject, args);
-        return (this._getSubject = function() { return subject; })();
-      };
-    },
-    klass: klass,
-    constructor: klass
-  });
-  
-  proxy.instanceMethod('extend', self.extend);
-  
-  return proxy;
-};
-
-JS.extend(JS.Proxy.Virtual, {
-  forward: function(name) {
-    return function() {
-      var subject = this._getSubject();
-      return subject[name].apply(subject, arguments);
-    };
+JS.Proxy.Virtual = new JS.Class({
+  initialize: function(klass) {
+    var bridge = function() {}, proxy = new JS.Class(),
+        method, func, delegators = {};
+    
+    bridge.prototype = klass.prototype;
+    
+    for (method in klass.prototype) {
+      func = klass.prototype[method];
+      if (JS.util.isFn(func) && func != klass) func = this.klass.forward(method);
+      delegators[method] = func;
+    }
+    
+    proxy.include({
+      initialize: function() {
+        var args = arguments, subject = null;
+        this._getSubject = function() {
+          subject = new bridge;
+          klass.apply(subject, args);
+          return (this._getSubject = function() { return subject; })();
+        };
+      },
+      klass: klass,
+      constructor: klass
+    });
+    
+    proxy.include(new JS.Module(delegators));
+    proxy.include(this.klass.InstanceMethods);
+    return proxy;
   },
   
-  extend: function(source) {
-    this._getSubject().extend(source);
-    var method, func;
-    for (method in source) {
-      func = source[method];
-      if (Function.is(func)) func = JS.Proxy.Virtual.forward(method);
-      this[method] = func;
-    }
+  extend: {
+    forward: function(name) {
+      return function() {
+        var subject = this._getSubject();
+        return subject[name].apply(subject, arguments);
+      };
+    },
+    
+    InstanceMethods: new JS.Module({
+      extend: function(source) {
+        this._getSubject().extend(source);
+        var method, func;
+        for (method in source) {
+          func = source[method];
+          if (JS.util.isFn(func)) func = JS.Proxy.Virtual.forward(method);
+          this[method] = func;
+        }
+      }
+    })
   }
 });
