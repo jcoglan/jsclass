jcoglan/jsclass

commit f0f4718b9ec9c33d2908b4dade1e2f0bd54188d3

Refactoring inheritance of class eigenmodules so that Class does not need its own __eigen__() method. Existing inheritance tests still pass, Class subclassing is still busted.

source/class.js
@@ -235,8 +235,12 @@ JS.ObjectMethods = new JS.Module({
 JS.Class = JS.makeFunction();
 JS.extend(JS.Class.prototype = JS.makeBridge(JS.Module), {
   initialize: function(parent, methods) {
-    var klass = JS.extend(JS.makeFunction(), this);
-    klass.klass = klass.constructor = JS.Class;
+    var klass = JS.makeFunction();
+    
+    klass.klass = klass.constructor = this.klass;
+    if (this.klass) this.__eigen__.call(klass).resolve();
+    else JS.extend(klass, this);
+    
     if (!JS.isFn(parent)) {
       methods = parent;
       parent = Object;
@@ -252,10 +256,17 @@ JS.extend(JS.Class.prototype = JS.makeBridge(JS.Module), {
   
   inherit: function(klass) {
     this.superclass = klass;
+    
+    if (this.__meta__) this.__meta__.include(klass.__eigen__
+        ? klass.__eigen__()
+        : new JS.Module(klass.prototype));
+    
     this.subclasses = [];
     (klass.subclasses || []).push(this);
+    
     var p = this.prototype = JS.makeBridge(klass);
     p.klass = p.constructor = this;
+    
     this.__mod__ = new JS.Module({}, {resolve: this.prototype});
     this.include(JS.ObjectMethods, null, false);
     this.include(klass.__mod__ || new JS.Module(klass.prototype, {resolve: klass.prototype}), null, false);
@@ -267,14 +278,6 @@ JS.extend(JS.Class.prototype = JS.makeBridge(JS.Module), {
     var mod = this.__mod__, options = options || {};
     options.included = this;
     return mod.include(module, options, resolve !== false);
-  },
-  
-  __eigen__: function() {
-    if (this.__meta__) return this.__meta__;
-    var module = this.callSuper();
-    var parent = this.superclass;
-    module.include(parent.__eigen__ ? parent.__eigen__() : new JS.Module(parent.prototype));
-    return module;
   },
   
   extend: function(module) {

commit 9ad4dfd8a09fb86b40abb629be53ec5d1f3f3f99

Fixing some performance problems introduced by f0f4718b..., and partially removing auto-dependent updating for dependencies more than one level deep (fc538f29...).

source/class.js
@@ -214,10 +214,6 @@ JS.extend(JS.Module.prototype, {
   resolve: function(target) {
     var target = target || this, resolved = target.__res__, i, n, key, made;
     
-    if (target == this) {
-      i = this.__dep__.length;
-      while (i--) this.__dep__[i].resolve();
-    }
     if (!resolved) return;
     
     for (i = 0, n = this.__inc__.length; i < n; i++)
@@ -255,12 +251,8 @@ JS.ObjectMethods = new JS.Module({
 JS.Class = JS.makeFunction();
 JS.extend(JS.Class.prototype = JS.makeBridge(JS.Module), {
   initialize: function(parent, methods) {
-    var klass = JS.makeFunction();
-    
+    var klass = JS.extend(JS.makeFunction(), this);
     klass.klass = klass.constructor = this.klass;
-    if (this.klass) this.__eigen__.call(klass).resolve();
-    else JS.extend(klass, this);
-    
     if (!JS.isFn(parent)) {
       methods = parent;
       parent = Object;
@@ -277,9 +269,12 @@ JS.extend(JS.Class.prototype = JS.makeBridge(JS.Module), {
   inherit: function(klass) {
     this.superclass = klass;
     
-    if (this.__meta__) this.__meta__.include(klass.__eigen__
-        ? klass.__eigen__()
-        : new JS.Module(klass.prototype));
+    if (this.__eigen__) {
+      this.__eigen__().include(klass.__eigen__
+          ? klass.__eigen__()
+          : new JS.Module(klass.prototype));
+      this.__meta__.resolve();
+    }
     
     this.subclasses = [];
     (klass.subclasses || []).push(this);
@@ -292,8 +287,6 @@ JS.extend(JS.Class.prototype = JS.makeBridge(JS.Module), {
     
     if (klass !== Object) this.include(klass.__mod__ || new JS.Module(klass.prototype,
         {resolve: klass.prototype}), null, false);
-    
-    this.extend();
   },
   
   include: function(module, options, resolve) {