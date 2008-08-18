856aed8114a73543e9fe7cd4c26ebf027a1f39be
James Coglan <jcoglan@googlemail.com>
2008-08-18 15:10:17 +0100

    Reworking the Compiler so it can compile Modules in a meaningful way. Rewrite from scratch, supporting complete ancestor lists for late-bound super method lookup.

--- source/compiler.js
+++ source/compiler.js
@@ -1,128 +1,122 @@
-JS.Compiler = {
-  queue: [],
-  
-  compile: function(klass, options) {
-    options = options || {};
-    var str = '', i, n;
-    this.queue.push(klass);
-    while (this.queue.length > 0)
-      str += new JS.ClassCompiler(this.queue.shift()).output();
-    if (options.subclasses) {
-      for (i = 0, n = klass.subclasses.length; i < n; i++)
-        str += this.compile(klass.subclasses[i], {subclasses: true});
-    }
-    return str;
-  },
-  
-  stringify: function(object, superMethod) {
-    switch (true) {
-      case (object === null):
-        return 'null';
-      case (object === undefined):
-        return 'undefined';
-      case (object.isA && object.isA(JS.Class)):
-        this.queue.push(object);
-        return null;
-      case (object instanceof Function):
-        return this.stringifyMethod(object, superMethod);
-      case (typeof object == 'number' || typeof object == 'boolean'):
-        return String(object);
-      case (typeof object == 'string'):
-        return '"' + object.replace(/"/g, "\\\"").replace(/\n/g, "\\n") + '"';
-    }
-  },
-  
-  stringifyMethod: function(method, superMethod) {
-    if (!JS.callsSuper(method)) return method.toString();
+JS.Compiler = new JS.Class({
+  extend: {
+    reset: function() {
+      this._queue = [];
+      this._processed = [];
+    },
     
-    return 'function() {\n' +
-    'var method = ' + method.toString() + ';\n' +
-    '    var $super = ' + superMethod + ',\n' +
-    '        args = [], i = arguments.length,\n' +
-    '        currentSuper = this.callSuper,\n' +
-    '        result;\n' +
-    '    \n' +
-    '    while (i--) args[i] = arguments[i];\n' +
-    '    \n' +
-    '    this.callSuper = function() {\n' +
-    '        var i = arguments.length;\n' +
-    '        while (i--) args[i] = arguments[i];\n' +
-    '        return $super.apply(this, args);\n' +
-    '    };\n' +
-    '    \n' +
-    '    result = method.apply(this, arguments);\n' +
-    '    currentSuper ? this.callSuper = currentSuper : delete this.callSuper;\n' +
-    '    return result;\n' +
-    '}';
-  }
-};
-
-JS.ClassCompiler = new JS.Class({
-  initialize: function(klass) {
-    this._subject = klass;
-    this._className = JS.StackTrace.nameOf(klass);
-    var superclass = this._subject.superclass;
-    this._superclass = (superclass == Object) ? 'Object' : JS.StackTrace.nameOf(superclass);
+    compile: function(klass, options) {
+      var str = '';
+      this._queue.push(klass);
+      while (this._queue.length > 0)
+        str += new this(this._queue.shift()).output(options || {});
+      return str;
+    },
+    
+    nameOf: function(object) {
+      return JS.StackTrace.nameOf(object);
+    },
+    
+    stringify: function(object) {
+      switch (true) {
+        case (object === null):
+          return 'null';
+        case (object === undefined):
+          return 'undefined';
+        case (object.isA && object.isA(JS.Class)):
+          this.queue.push(object);
+          return null;
+        case (object instanceof Function):
+          return object.toString().replace(/^\s*/g, '').replace(/\s*$/g, '');
+        case (typeof object == 'number' || typeof object == 'boolean'):
+          return String(object);
+        case (typeof object == 'string'):
+          return '"' + object.replace(/"/g, "\\\"").replace(/\n/g, "\\n") + '"';
+      }
+    }
+  },
+  
+  initialize: function(classOrModule) {
+    if (!classOrModule.isA) return this;
+    if (classOrModule.isA(JS.Class)) return new JS.ClassCompiler(classOrModule);
+    if (classOrModule.isA(JS.Module)) return new JS.ModuleCompiler(classOrModule);
   },
   
   output: function() {
-    return this.declaration() + this.classMethods() + this.instanceMethods();
+    return '/* Could not be compiled -- not a class or module */\n';
+  }
+});
+
+JS.Compiler.reset();
+
+JS.Compiler.extend({superCaller: JS.Compiler.stringify(function() {
+  var self = arguments.callee, anc = self.__anc__, nym = self.__nym__,
+      methods = [], i = arguments.length, n, found, params = [];
+  
+  while (i--) params.push(arguments[i]);
+  
+  for (i = 0, n = anc.length; i < n; i++) {
+    if (found = anc[i].__fns__[nym]) methods.push(found);
+  }
+  var stackIndex = methods.length,
+      currentSuper = this.callSuper,
+      that = this, result;
+  
+  this.callSuper = function() {
+    var i = arguments.length;
+    while (i--) params[i] = arguments[i];
+    stackIndex -= 1;
+    var returnValue = methods[stackIndex].apply(that, params);
+    stackIndex += 1;
+    return returnValue;
+  };
+  
+  result = this.callSuper();
+  currentSuper ? this.callSuper = currentSuper : delete this.callSuper;
+  return result;
+}) });
+
+JS.ModuleCompiler = new JS.Class(JS.Compiler, {
+  initialize: function(module) {
+    this._module = module;
+    this._name = this.klass.nameOf(module);
+  },
+  
+  output: function() {
+    return this.declaration() + this.singletonMethods();
   },
   
   declaration: function() {
-    var str = this._className + ' = ' + this._subject.toString() + ';\n';
-    if (this._subject.superclass != Object) str += this.subclass();
-    str += this._className + '.prototype.constructor = \n';
-    str += this._className + '.prototype.klass = ' + this._className + ';\n';
-    str += this._className + '.superclass = ' + this._superclass + ';\n';
-    str += this._className + '.subclasses = [];\n';
-    if (this._subject.superclass.subclasses instanceof Array)
-      str += this._superclass + '.subclasses.push(' + this._className + ');\n';
-    return str;
+    return  this._name + ' = {__fns__: {}, __meta__: {__fns__: {}}};\n';
   },
   
-  subclass: function() {
-    return this._className + '.prototype = (function() {\n' +
-    '    var bridge = function() {};\n' +
-    '    bridge.prototype = ' + this._superclass + '.prototype;\n' +
-    '    return new bridge;\n' +
-    '})();\n';
-  },
-  
-  classMethods: function() {
-    var anc = this._subject.__eigen__().ancestors(),
-        methods = {},
-        str = '',
-        value, method,
-        superMethod;
+  singletonMethods: function() {
+    var ancestors = this._module.__eigen__().ancestors().slice(2), self = ancestors.pop(),
+        i, n, name, block, method, str = '', assign;
     
-    for (var i = 0, n = anc.length; i < n; i++) {
-      if (anc[i] == JS.ObjectMethods || anc[i] == JS.Module || anc[i] == JS.Class) continue;
-      JS.extend(methods, anc[i].__mod__.__fns__);
-    }
-    for (method in methods) {
-      if (methods[method] === this._subject.superclass[method]) {
-        str += this._className + '.' + method + ' = ' + this._superclass + '.' + method + ';\n';
-        continue;
+    block = self.__fns__;
+    
+    for (method in block) {
+      assign = this._name + '.' + method;
+      if (!JS.callsSuper(block[method])) {
+        str += assign + ' = ' + this.klass.stringify(block[method]) + ';\n';
+      } else {
+        str += this._name + '.__meta__.__fns__.' + method + ' = ' + this.klass.stringify(block[method]) + ';\n';
+        str += assign + ' = ' + this.klass.superCaller + ';\n';
+        str += assign + '.__anc__ = [' + this.klass.nameOf(ancestors).join(', ') + ', ' + this._name + '.__meta__];\n';
+        str += assign + '.__nym__ = ' + this.klass.stringify(method) + ';\n';
       }
-      superMethod = this._superclass + '.' + method;
-      value = JS.Compiler.stringify(methods[method], superMethod);
-      if (!value) continue;
-      str += this._className + '.' + method + ' = ' + value + ';\n';
     }
-    return str;
-  },
-  
-  instanceMethods: function() {
-    var anc = this._subject.ancestors(), methods = {}, str = '', method, superMethod;
-    for (var i = 0, n = anc.length; i < n; i++) {
-      if (anc[i] == JS.ObjectMethods) continue;
-      JS.extend(methods, anc[i].__mod__.__fns__);
-    }
-    for (method in methods) {
-      if (methods[method] === this._subject.superclass.prototype[method]) continue;
-      superMethod = this._superclass + '.prototype.' + method;
-      str += this._className + '.prototype.' + method + ' = ' + JS.Compiler.stringify(methods[method], superMethod) + ';\n';
+    
+    for (i = 0, n = ancestors.length; i < n; i++) {
+      if (ancestors[i] == JS.ObjectMethods || ancestors[i] == JS.Module) continue;
+      name = this.klass.nameOf(ancestors[i]);
+      console.log(name);
+      block = ancestors[i].__fns__;
+      for (method in block) {
+        if (this._module[method] === block[method])
+          str += this._name + '.' + method + ' = ' + name + '.__fns__.' + method + ';\n';
+      }
     }
     return str;
   }
