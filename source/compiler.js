b9707b38bad9cd4f7881531844f2125d76b9cdf4
James Coglan <jcoglan@googlemail.com>
2008-08-22 15:04:42 +0100

    Cleaning up the Compiler structure: each Compiler instance compiles a single module, and the dependency list is expanded completely first before any code generation is done.

--- source/compiler.js
+++ source/compiler.js
@@ -1,15 +1,53 @@
 JS.Compiler = new JS.Class({
   extend: {
+    EXCLUDED: [JS.ObjectMethods, JS.Module, JS.Class],
+    
     reset: function() {
-      this.extend({ _queue: [], _processed: [] });
+      this.extend({ _queue: [] });
     },
     
-    compile: function(klass, options) {
-      var str = '';
-      this._queue.push(klass);
-      while (this._queue.length > 0)
-        str += new JS.Compiler(this._queue.shift()).output(options || {});
+    compile: function() {
+      var list = JS.array(arguments),
+          i = list.length - 1,
+          options = list[i].isA ? {} : list.pop();
+      var str = '', list = this.expand(list, options || {}), n;
+      for (i = 0, n = list.length; i < n; i++)
+        str += new JS.Compiler(list[i]).output();
       return str;
+    },
+    
+    expand: function(list, options) {
+      var length = 0, module, temp, expansion, i, j, n, m;
+      while (length != list.length) {
+        length = list.length;
+        temp = [];
+        for (i = 0, n = list.length; i < n; i++) {
+          module = list[i];
+          expansion = this.expandSingle(module, options);
+          for (j = 0, m = expansion.length; j < m; j++) {
+            if (JS.indexOf(temp, expansion[j]) != -1) continue;
+            if (JS.indexOf(this.EXCLUDED, expansion[j]) != -1) continue;
+            temp.push(expansion[j]);
+          }
+        }
+        list = temp;
+      }
+      return list;
+    },
+    
+    expandSingle: function(module, options) {
+      var list = [module], key, eigen = module.__eigen__().__fns__;
+      if (options.nested) {
+        for (key in eigen) {
+          if (eigen[key] && eigen[key].isA && eigen[key].isA(JS.Module))
+            list.push(eigen[key]);
+        }
+      }
+      if (options.dependencies) {
+        list = module.ancestors().slice(0,-1).concat(
+            module.__eigen__().ancestors().slice(0,-1), list);
+      }
+      return list;
     },
     
     nameOf: function(object) {
@@ -37,8 +75,6 @@ JS.Compiler = new JS.Class({
   
   initialize: function(classOrModule) {
     if (!classOrModule.isA) return this;
-    if (JS.indexOf(this.klass._processed, classOrModule) != -1) return this;
-    this.klass._processed.push(classOrModule);
     if (classOrModule.isA(JS.Class)) return new JS.ClassCompiler(classOrModule);
     if (classOrModule.isA(JS.Module)) return new JS.ModuleCompiler(classOrModule);
   },
@@ -50,33 +86,6 @@ JS.Compiler = new JS.Class({
 
 JS.Compiler.reset();
 
-JS.Compiler.extend({superCaller: JS.Compiler.stringify(function() {
-  var self = arguments.callee, anc = self.__anc__, nym = self.__nym__,
-      methods = [], i = arguments.length, n, found, params = [];
-  
-  while (i--) params.push(arguments[i]);
-  
-  for (i = 0, n = anc.length; i < n; i++) {
-    if (found = anc[i].__fns__[nym]) methods.push(found);
-  }
-  var stackIndex = methods.length,
-      currentSuper = this.callSuper,
-      that = this, result;
-  
-  this.callSuper = function() {
-    var i = arguments.length;
-    while (i--) params[i] = arguments[i];
-    stackIndex -= 1;
-    var returnValue = methods[stackIndex].apply(that, params);
-    stackIndex += 1;
-    return returnValue;
-  };
-  
-  result = this.callSuper();
-  currentSuper ? this.callSuper = currentSuper : delete this.callSuper;
-  return result;
-}) });
-
 JS.ModuleCompiler = new JS.Class(JS.Compiler, {
   initialize: function(module) {
     this._module = module;
@@ -84,14 +93,7 @@ JS.ModuleCompiler = new JS.Class(JS.Compiler, {
   },
   
   output: function(options) {
-    var str = this.declaration() + this.singletonMethods() + this.instanceMethods(),
-        anc, meta, self, mod;
-    if (options.dependencies) {
-      anc = this._module.ancestors(); self = anc.pop();
-      meta = this._module.__eigen__ ? this._module.__eigen__().ancestors().slice(2) : []; self = meta.pop();
-      while (mod = anc.shift()) str = this.klass.compile(mod, options) + str;
-      while (mod = meta.shift()) str = this.klass.compile(mod, options) + str;
-    }
+    var str = this.declaration() + this.singletonMethods() + this.instanceMethods();
     return str;
   },
   
@@ -107,8 +109,8 @@ JS.ModuleCompiler = new JS.Class(JS.Compiler, {
     
     for (method in block) {
       assign = this._name + '.' + method;
-      if (block[method].isA && (block[method].isA(JS.Module) || block[method].isA(JS.Class))) {
-        str += JS.Compiler.compile(block[method]);
+      if (block[method].isA && block[method].isA(JS.Module)) {
+        continue;
       } else if (!JS.callsSuper(block[method])) {
         str += assign + ' = ' + this.klass.stringify(block[method]) + ';\n';
       } else {
@@ -138,4 +140,31 @@ JS.ModuleCompiler = new JS.Class(JS.Compiler, {
     return str;
   }
 });
+
+JS.Compiler.extend({superCaller: JS.Compiler.stringify(function() {
+  var self = arguments.callee, anc = self.__anc__, nym = self.__nym__,
+      methods = [], i = arguments.length, n, found, params = [];
+  
+  while (i--) params.push(arguments[i]);
+  
+  for (i = 0, n = anc.length; i < n; i++) {
+    if (found = anc[i].__fns__[nym]) methods.push(found);
+  }
+  var stackIndex = methods.length,
+      currentSuper = this.callSuper,
+      that = this, result;
+  
+  this.callSuper = function() {
+    var i = arguments.length;
+    while (i--) params[i] = arguments[i];
+    stackIndex -= 1;
+    var returnValue = methods[stackIndex].apply(that, params);
+    stackIndex += 1;
+    return returnValue;
+  };
+  
+  result = this.callSuper();
+  currentSuper ? this.callSuper = currentSuper : delete this.callSuper;
+  return result;
+}) });
 
