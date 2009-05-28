9fff0a5ddbc1208bd15c935f9912949002a9418e
James Coglan <jcoglan@googlemail.com>
2009-05-28 15:42:36 +0100

    Format package config file.

--- source/package_config.js
+++ source/package_config.js
@@ -1,65 +1,74 @@
 JS.Packages(function() { with(this) {
-    file(JSCLASS_PATH + 'core.js')
-        .provides('JS.Module', 'JS.Class', 'JS.Kernel', 'JS.Singleton', 'JS.Interface');
+    var module = function(name) { return file(JSCLASS_PATH + name + '.js') };
     
-    file(JSCLASS_PATH + 'comparable.js')
-        .provides('JS.Comparable')
-        .requires('JS.Module');
+    module('core')          .provides('JS.Module',
+                                      'JS.Class',
+                                      'JS.Kernel',
+                                      'JS.Singleton',
+                                      'JS.Interface');
     
-    file(JSCLASS_PATH + 'constant_scope.js')
-        .provides('JS.ConstantScope')
-        .requires('JS.Module');
+    module('comparable')    .provides('JS.Comparable')
+                            .requires('JS.Module');
     
-    file(JSCLASS_PATH + 'forwardable.js')
-        .provides('JS.Forwardable')
-        .requires('JS.Module');
+    module('constant_scope').provides('JS.ConstantScope')
+                            .requires('JS.Module');
     
-    file(JSCLASS_PATH + 'enumerable.js')
-        .provides('JS.Enumerable')
-        .requires('JS.Module', 'JS.Class');
+    module('forwardable')   .provides('JS.Forwardable')
+                            .requires('JS.Module');
     
-    file(JSCLASS_PATH + 'observable.js')
-        .provides('JS.Observable')
-        .requires('JS.Module');
+    module('enumerable')    .provides('JS.Enumerable')
+                            .requires('JS.Module',
+                                      'JS.Class');
     
-    file(JSCLASS_PATH + 'hash.js')
-        .provides('JS.Hash')
-        .requires('JS.Class', 'JS.Enumerable', 'JS.Comparable');
+    module('observable')    .provides('JS.Observable')
+                            .requires('JS.Module');
     
-    file(JSCLASS_PATH + 'set.js')
-        .provides('JS.Set', 'JS.HashSet', 'JS.SortedSet')
-        .requires('JS.Class', 'JS.Enumerable', 'JS.Hash');
+    module('hash')          .provides('JS.Hash')
+                            .requires('JS.Class',
+                                      'JS.Enumerable',
+                                      'JS.Comparable');
     
-    file(JSCLASS_PATH + 'linked_list.js')
-        .provides('JS.LinkedList', 'JS.LinkedList.Doubly', 'JS.LinkedList.Doubly.Circular')
-        .requires('JS.Class', 'JS.Enumerable');
+    module('set')           .provides('JS.Set',
+                                      'JS.HashSet',
+                                      'JS.SortedSet')
+                            .requires('JS.Class',
+                                      'JS.Enumerable',
+                                      'JS.Hash');
     
-    file(JSCLASS_PATH + 'command.js')
-        .provides('JS.Command', 'JS.Command.Stack')
-        .requires('JS.Class', 'JS.Enumerable', 'JS.Observable');
+    module('linked_list')   .provides('JS.LinkedList',
+                                      'JS.LinkedList.Doubly',
+                                      'JS.LinkedList.Doubly.Circular')
+                            .requires('JS.Class',
+                                      'JS.Enumerable');
     
-    file(JSCLASS_PATH + 'decorator.js')
-        .provides('JS.Decorator')
-        .requires('JS.Module', 'JS.Class');
+    module('command')       .provides('JS.Command',
+                                      'JS.Command.Stack')
+                            .requires('JS.Class',
+                                      'JS.Enumerable',
+                                      'JS.Observable');
     
-    file(JSCLASS_PATH + 'method_chain.js')
-        .provides('JS.MethodChain')
-        .requires('JS.Module', 'JS.Kernel');
+    module('decorator')     .provides('JS.Decorator')
+                            .requires('JS.Module',
+                                      'JS.Class');
     
-    file(JSCLASS_PATH + 'proxy.js')
-        .provides('JS.Proxy', 'JS.Proxy.Virtual')
-        .requires('JS.Module', 'JS.Class');
+    module('method_chain')  .provides('JS.MethodChain')
+                            .requires('JS.Module',
+                                      'JS.Kernel');
     
-    file(JSCLASS_PATH + 'ruby.js')
-        .provides('JS.Ruby')
-        .requires('JS.Class');
+    module('proxy')         .provides('JS.Proxy',
+                                      'JS.Proxy.Virtual')
+                            .requires('JS.Module',
+                                      'JS.Class');
     
-    file(JSCLASS_PATH + 'stack_trace.js')
-        .provides('JS.StackTrace')
-        .requires('JS.Module', 'JS.Singleton');
+    module('ruby')          .provides('JS.Ruby')
+                            .requires('JS.Class');
     
-    file(JSCLASS_PATH + 'state.js')
-        .provides('JS.State')
-        .requires('JS.Module', 'JS.Class');
+    module('stack_trace')   .provides('JS.StackTrace')
+                            .requires('JS.Module',
+                                      'JS.Singleton');
+    
+    module('state')         .provides('JS.State')
+                            .requires('JS.Module',
+                                      'JS.Class');
 }});
 
