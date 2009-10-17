f7d108021a55fcd892bdd2a1da1cfb63bd610b13
James Coglan <jcoglan@googlemail.com>
2009-10-17 21:57:16 +0100

    Spec files should not contain require() blocks since this will stop the package loader from verifying their contents when loading them asynchronously in a browser.

--- test/runner.js
+++ test/runner.js
@@ -3,6 +3,8 @@ load(JSCLASS_PATH + 'loader.js');
 
 JS.Packages(function() { with(this) {
   autoload(/^.*Spec$/, {from: 'test/specs'});
+  
+  pkg('ModuleSpec').requires('JS.Module');
 }});
 
 require('JS.Test', function() {
