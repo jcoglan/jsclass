225d7cb67bb2066d3a5d6e53ea6bba0abc01382f
James Coglan <jcoglan@gmail.com>
2010-08-15 18:09:47 +0100

    Specs for argument-passing and method lookup in callSuper().

--- test/specs/class_spec.js
+++ test/specs/class_spec.js
@@ -63,5 +63,82 @@ ClassSpec = JS.Test.describe(JS.Class, function() {
       assertEqual( "kermit likes miss piggy", kermit.speak("miss piggy") )
     })
   })
+  
+  describe("#callSuper", function() {
+    before(function() {
+      this.Parent = new JS.Class({
+        aMethod: function(thing, stuff) { return thing + ", " + stuff }
+      })
+    })
+    
+    describe("calling the superclass with implicit args", function() {
+      before(function() {
+        this.Child = new JS.Class(Parent, {
+          aMethod: function() { return this.callSuper() }
+        })
+      })
+      
+      it("passes args through to the parent method", function() {
+        var object = new Child()
+        assertEqual( "foo, bar", object.aMethod("foo", "bar") )
+      })
+    })
+    
+    describe("calling the superclass with one explicit arg", function() {
+      before(function() {
+        this.Child = new JS.Class(Parent, {
+          aMethod: function(a) { return this.callSuper(a.toUpperCase()) }
+        })
+      })
+      
+      it("passes the remaining implicit args after the explicit one", function() {
+        var object = new Child()
+        assertEqual( "FOO, bar", object.aMethod("foo", "bar") )
+      })
+    })
+    
+    describe("calling the superclass with explicit args", function() {
+      before(function() {
+        this.Child = new JS.Class(Parent, {
+          aMethod: function(a,b) { return this.callSuper(a.toUpperCase(), b.toUpperCase()) }
+        })
+      })
+      
+      it("passes args through to the parent method", function() {
+        var object = new Child()
+        assertEqual( "FOO, BAR", object.aMethod("foo", "bar") )
+      })
+    })
+    
+    describe("calling methods from mixins", function() {
+      before(function() {
+        this.modA = new JS.Module({ aMethod: function() { return "A" } })
+        
+        this.modB = new JS.Module({
+          include: modA,
+          aMethod: function() { return this.callSuper() + ", B" }
+        })
+        
+        this.modC = new JS.Module({
+          aMethod: function() { return this.callSuper() + ", C" }
+        })
+        
+        this.modD = new JS.Module({
+          include: modA,
+          aMethod: function() { return this.callSuper() + ", D" }
+        })
+        
+        this.classE = new JS.Class({
+          include: [modB, modC, modD],
+          aMethod: function() { return this.callSuper() + ", E" }
+        })
+      })
+      
+      it("uses the object's ancestry to route each super() call", function() {
+        var object = new classE()
+        assertEqual( "A, B, C, D, E", object.aMethod() )
+      })
+    })
+  })
 })
 
