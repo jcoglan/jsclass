5d302172cef18ab1374ed8e311388813bcf8d1d6
Aurelio A. Heckert <aurium@gmail.com>
2010-10-02 14:25:08 -0300

    Support to mozIJSSubScriptLoader on JS.Package.DomLoader.loadFile() to work on XUL applications

--- source/package/loader.js
+++ source/package/loader.js
@@ -9,25 +9,42 @@ JS.Package.DomLoader = {
   },
   
   loadFile: function(path, fireCallbacks) {
-    var self = this,
-        tag = document.createElement('script');
-    
-    tag.type = 'text/javascript';
-    tag.src = path;
-    
-    tag.onload = tag.onreadystatechange = function() {
-      var state = tag.readyState, status = tag.status;
-      if ( !state || state === 'loaded' || state === 'complete' || (state === 4 && status === 200) ) {
-        fireCallbacks();
-        tag.onload = tag.onreadystatechange = self._K;
-        tag = null;
-      }
-    };
-    
     if (window.console && console.info)
       console.info('Loading ' + path);
     
-    document.getElementsByTagName('head')[0].appendChild(tag);
+    var loader = false;
+    if ( Components &&
+         Components.classes &&
+         Components.classes["@mozilla.org/moz/jssubscript-loader;1"] &&
+         Components.classes["@mozilla.org/moz/jssubscript-loader;1"].getService ) {
+      loader = Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
+                         .getService(Components.interfaces.mozIJSSubScriptLoader);
+    }
+
+    if ( loader ) {
+      alert(path)
+      loader.loadSubScript(path);
+      fireCallbacks();
+    
+    } else {
+    
+      var self = this,
+      tag = document.createElement('script');
+      
+      tag.type = 'text/javascript';
+      tag.src = path;
+      
+      tag.onload = tag.onreadystatechange = function() {
+        var state = tag.readyState, status = tag.status;
+        if ( !state || state === 'loaded' || state === 'complete' || (state === 4 && status === 200) ) {
+          fireCallbacks();
+          tag.onload = tag.onreadystatechange = self._K;
+          tag = null;
+        }
+      };
+      
+      document.getElementsByTagName('head')[0].appendChild(tag);
+    }
   },
   
   _K: function() {}
