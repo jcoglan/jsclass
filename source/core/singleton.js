2001c7ed3aaee21d09c5a328a3864cd12d7f297e
James Coglan <jcoglan@gmail.com>
2010-10-10 23:41:41 +0100

    Give Interface and Singleton class names so their tests don't break the browser test run.

--- source/core/singleton.js
+++ source/core/singleton.js
@@ -1,21 +1,4 @@
-/** section: core
- * class JS.Singleton
- * 
- * `Singleton` is a class used to construct custom objects with all the inheritance features
- * of `JS.Class`, the methods from `JS.Kernel`, etc. It constructs an anonymous class from the
- * objects you provide and returns an instance of this class.
- **/
-JS.Singleton = new JS.Class({
-  /**
-   * new JS.Singleton(name, parent, methods)
-   * - name (String): the name of the singleton, used for debugging
-   * - parent (JS.Class): the parent class to inherit from
-   * - methods (Object): list of methods for the singleton
-   * 
-   * `Singleton`s are instantiated the same way as instances of `JS.Class`, the only difference
-   * being that `Singleton` returns an instance of the newly created class, rather than the
-   * class itself.
-   **/
+JS.Singleton = new JS.Class('Singleton', {
   initialize: function(name, parent, methods) {
     return new (new JS.Class(name, parent, methods));
   }
