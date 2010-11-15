3397cb9f7472af960fe2feb87c986a280d050e76
James Coglan <jcoglan@gmail.com>
2010-11-15 00:47:22 +0000

    For benchmarking, construct all the environments we'll need *before* running the tests, so we don't get huge errors from calculating the tiny difference between the total time and setup time.

--- test/benchmarks/benchmark.js
+++ test/benchmarks/benchmark.js
@@ -2,52 +2,38 @@ Benchmark = {
   N: 5,
   
   measure: function(name, runs, functions) {
-    var times = {setup: [], test: []};
+    var envs  = [], env,
+        times = [],
+        block = functions.test;
     
-    this.collectTimes(runs, times.setup, function() {
-      var context = {};
-      if (functions.setup) functions.setup.call(context);
-    });
+    var i = runs * this.N;
+    while (i--) {
+      env = {};
+      if (functions.setup) functions.setup.call(env);
+      envs.push(env);
+    }
     
-    this.collectTimes(runs, times.test, function() {
-      var context = {};
-      if (functions.setup) functions.setup.call(context);
-      functions.test.call(context);
-    });
-    
-    var setup = this.average(times.setup),
-        test  = this.average(times.test),
-        total = this.difference(test, setup);
-    
-    print(this.format(total));
-  },
-  
-  collectTimes: function(runs, results, block) {
-    var n = this.N, start, end, i;
+    var n = this.N, start, end;
     while (n--) {
       i = runs;
       start = new Date().getTime();
-      while (i--) block.call();
+      while (i--) block.call(envs.pop());
       end = new Date().getTime();
-      results.push(end - start);
+      times.push(end - start);
     }
-  },
-  
-  average: function(list) {
-    return { value: this.mean(list), error: this.stddev(list) };
-  },
-  
-  difference: function(a, b) {
-    return {
-      value: a.value - b.value,
-      error: Math.sqrt(Math.pow(a.error, 2) + Math.pow(b.error, 2))
-    };
+    
+    var average = this.average(times);
+    print(name + ' :: ' + this.format(average));
   },
   
   format: function(average) {
     var error = (average.value === 0) ? 0 : 100 * average.error / average.value;
     return Math.round(average.value) +
            ' +/- ' + Math.round(error) + '%';
+  },
+  
+  average: function(list) {
+    return { value: this.mean(list), error: this.stddev(list) };
   },
   
   mean: function(list, mapper) {
@@ -64,8 +50,8 @@ Benchmark = {
   },
   
   stddev: function(list) {
-    return Math.sqrt(this.mean(list, function(x) { return x*x }) -
-                     Math.pow(this.mean(list), 2));
+    var square = function(x) { return x*x };
+    return Math.sqrt(this.mean(list, square) - square(this.mean(list)));
   }
 };
 
