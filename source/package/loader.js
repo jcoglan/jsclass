7237625fa22e97f81f9331eb7e9967c903ca177c
James Coglan <jcoglan@gmail.com>
2011-01-28 16:58:01 +0000

    Load stylesheets dynamically on XULRunner.

--- source/package/loader.js
+++ source/package/loader.js
@@ -46,33 +46,37 @@ JS.Package.DomLoader = {
 
 
 JS.Package.MozIJSSubScriptLoader = {
+  jsloader:   '@mozilla.org/moz/jssubscript-loader;1',
+  cssservice: '@mozilla.org/content/style-sheet-service;1',
+  ioservice:  '@mozilla.org/network/io-service;1',
+  
   usable: function() {
     try {
-      if ( Components &&
-           Components.classes &&
-           Components.classes['@mozilla.org/moz/jssubscript-loader;1'] &&
-           Components.classes['@mozilla.org/moz/jssubscript-loader;1'].getService ) {
-        return true;
-      } else {
-        return false;
-      }
+      var CC = (Components || {}).classes;
+      return !!(CC && CC[this.jsloader] && CC[this.jsloader].getService);
     } catch(e) {
       return false;
     }
   },
 
   setup: function() {
-    this.mozLoader = Components.
-                     classes['@mozilla.org/moz/jssubscript-loader;1'].
-                     getService(Components.interfaces.mozIJSSubScriptLoader);
+    var Cc = Components.classes, Ci = Components.interfaces;
+    this.ssl = Cc[this.jsloader].getService(Ci.mozIJSSubScriptLoader);
+    this.sss = Cc[this.cssservice].getService(Ci.nsIStyleSheetService);
+    this.ios = Cc[this.ioservice].getService(Ci.nsIIOService);
   },
 
   loadFile: function(path, fireCallbacks) {
     if (window.console && console.info)
       console.info('Loading ' + path);
     
-    this.mozLoader.loadSubScript(path);
+    this.ssl.loadSubScript(path);
     fireCallbacks();
+  },
+  
+  loadStyle: function(path) {
+    var uri = this.ios.newURI(path, null, null);
+    this.sss.loadAndRegisterSheet(uri, this.sss.USER_SHEET);
   }
 };
 
