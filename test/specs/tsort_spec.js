d3539aef29aec3701e17bde233a9b3f7bbac4d65
James Coglan <jcoglan@gmail.com>
2011-01-30 17:46:54 +0000

    Test that TSort works with object data.

--- test/specs/tsort_spec.js
+++ test/specs/tsort_spec.js
@@ -15,37 +15,75 @@ TSortSpec = JS.Test.describe(JS.TSort, function() {
     })
   })
   
-  describe("with no cycles", function() {
-    before(function() {
-      this.hash = new Hash([ 1, [2,3],
-                             2, [3],
-                             3, [],
-                             4, [] ])
+  describe("with primitive data", function() {
+    describe("with no cycles", function() {
+      before(function() {
+        this.hash = new Hash([ 1, [2,3],
+          2, [3],
+          3, [],
+          4, [] ])
+        })
+        
+      it("sorts the elements topologically", function() {
+        assertEqual( [3,2,1,4], hash.tsort() )
+      })
+      
+      it("identifies strongly connected nodes", function() {
+        assertEqual( [[3],[2],[1],[4]], hash.stronglyConnectedComponents() )
+      })
     })
     
-    it("sorts the elements topologically", function() {
-      assertEqual( [3,2,1,4], hash.tsort() )
-    })
-    
-    it("identifies strongly connected nodes", function() {
-      assertEqual( [[3],[2],[1],[4]], hash.stronglyConnectedComponents() )
+    describe("when there are cycles", function() {
+      before(function() {
+        this.hash = new Hash([ 1, [2,3,4],
+          2, [3],
+          3, [2],
+          4, [1] ])
+        })
+        
+      it("raises an error", function() {
+        assertThrows(JS.TSort.Cyclic, function() { hash.tsort() })
+      })
+      
+      it("identifies strongly connected nodes", function() {
+        assertEqual( [[2,3],[1,4]], hash.stronglyConnectedComponents() )
+      })
     })
   })
   
-  describe("when there are cycles", function() {
+  describe("with object data", function() {
+    include(JS.Test.Helpers)
+    
     before(function() {
-      this.hash = new Hash([ 1, [2,3,4],
-                             2, [3],
-                             3, [2],
-                             4, [1] ])
+      this.TodoItem = new JS.Class("TodoItem", {
+        initialize: function(priority) {
+          this.priority = priority
+        },
+        equals: function(other) {
+          return this.priority == other.priority
+        },
+        hash: function() {
+          return this.priority
+        }
+      })
     })
     
-    it("raises an error", function() {
-      assertThrows(JS.TSort.Cyclic, function() { hash.tsort() })
-    })
-    
-    it("identifies strongly connected nodes", function() {
-      assertEqual( [[2,3],[1,4]], hash.stronglyConnectedComponents() )
+    describe("with no cycles", function() {
+      before(function() {
+        this.hash = new Hash([ new TodoItem(1), [new TodoItem(2),new TodoItem(3)],
+          new TodoItem(2), [new TodoItem(3)],
+          new TodoItem(3), [],
+          new TodoItem(4), [] ])
+        })
+        
+      it("sorts the elements topologically", function() {
+        assertEqual( [3,2,1,4], map(hash.tsort(), "priority") )
+      })
+      
+      it("identifies strongly connected nodes", function() {
+        assertEqual( [[new TodoItem(3)],[new TodoItem(2)],[new TodoItem(1)],[new TodoItem(4)]],
+                     hash.stronglyConnectedComponents() )
+      })
     })
   })
 })
