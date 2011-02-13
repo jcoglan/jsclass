d48bc7a79869a4340bfd242090a4839488bc9ae3
James Coglan <jcoglan@gmail.com>
2011-02-13 17:39:16 +0000

    Remove setup() method from ServerLoader.

--- source/package/loaders/server.js
+++ source/package/loaders/server.js
@@ -4,21 +4,12 @@ JS.Package.ServerLoader = {
            typeof JS.Package.getObject('version') === 'function';
   },
   
-  setup: function() {
-    var self = this;
-    load = (function(origLoad) {
-      return function() {
-        self._currentPath = arguments[0];
-        return origLoad.apply(JS.Package.ENV, arguments);
-      };
-    })(load);
-  },
-  
   __FILE__: function() {
     return this._currentPath;
   },
   
   loadFile: function(path, fireCallbacks) {
+    this._currentPath = path;
     load(path);
     fireCallbacks();
   }
