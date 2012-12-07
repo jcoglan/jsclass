f780e0eaf6616513ecefc0774d46cb0be5af524f
James Coglan <jcoglan@gmail.com>
2012-12-07 11:41:22 +0000

    Remove semicolons from test runner.

--- test/console.js
+++ test/console.js
@@ -1,13 +1,13 @@
 if (this.ActiveXObject) load = function(path) {
-  var fso = new ActiveXObject('Scripting.FileSystemObject'), file, runner;
+  var fso = new ActiveXObject('Scripting.FileSystemObject'), file, runner
   try {
-    file   = fso.OpenTextFile(path);
-    runner = function() { eval(file.ReadAll()) };
-    runner();
+    file   = fso.OpenTextFile(path)
+    runner = function() { eval(file.ReadAll()) }
+    runner()
   } finally {
     try { if (file) file.Close() } catch (e) {}
   }
-};
+}
 
 (function() {
   var $ = (typeof global === 'object') ? global : this
