81ff6a5ef500eba6d62c8bcc93b21e1dec5bdfc4
James Coglan <jcoglan@gmail.com>
2012-12-07 18:29:48 +0000

    Bit of a hack to load files from another server under Testacular. Still doesn't work.

--- test/runner.js
+++ test/runner.js
@@ -1,6 +1,7 @@
 JS.ENV.CWD = (typeof CWD === 'undefined') ? '.' : CWD
 
 JS.cacheBust = true
+if (JS.ENV.JS_DEBUG) JS.debug = true
 
 JS.Packages(function() { with(this) {
     autoload(/^(.*)Spec$/, {from: CWD + '/test/specs', require: 'JS.$1'})
--- test/testacular.js
+++ test/testacular.js
@@ -1,11 +1,17 @@
-basePath        = '..';
-files           = ['build/min/loader-browser.js', 'test/runner.js'];
-reporters       = ['progress'];
-port            = 8080;
-runnerPort      = 9100;
-colors          = true;
-logLevel        = LOG_INFO;
-autoWatch       = true;
-captureTimeout  = 5000;
-singleRun       = false;
+CWD = 'http://localhost:8000'
+JSCLASS_PATH = CWD + '/build/min'
+JS_DEBUG = true
+
+if (typeof window === 'undefined') {
+  basePath        = '..'
+  files           = ['test/testacular.js', JSCLASS_PATH + '/loader-browser.js', 'test/runner.js']
+  reporters       = ['progress']
+  port            = 8080
+  runnerPort      = 9100
+  colors          = true
+  logLevel        = LOG_INFO
+  autoWatch       = true
+  captureTimeout  = 5000
+  singleRun       = false
+}
 
