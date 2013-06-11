79270ad0d3c684b1661f2beef2cb6fc108cbbafe
James Coglan <jcoglan@gmail.com>
2013-06-11 19:51:32 +0100

    Add ability to re-order reporter events to Composite.

--- source/test/reporters/composite.js
+++ source/test/reporters/composite.js
@@ -2,6 +2,8 @@ Test.Reporters.extend({
   Composite: new JS.Class({
     initialize: function(reporters) {
       this._reporters = reporters || [];
+      this._queue     = [];
+      this._pointer   = 0;
     },
 
     addReporter: function(reporter) {
@@ -12,6 +14,19 @@ Test.Reporters.extend({
     removeReporter: function(reporter) {
       var index = JS.indexOf(this._reporters, reporter);
       if (index >= 0) this._reporters.splice(index, 1);
+    },
+
+    flush: function() {
+      var queue = this._queue, method, event, i, n, fn;
+      while (queue[this._pointer] !== undefined) {
+        method = queue[this._pointer][0];
+        event =  queue[this._pointer][1];
+        for (i = 0, n = this._reporters.length; i < n; i++) {
+          fn = this._reporters[i][method];
+          if (fn) fn.call(this._reporters[i], event);
+        }
+        this._pointer += 1;
+      }
     }
   })
 });
@@ -24,11 +39,8 @@ Test.Reporters.extend({
     (function(i) {
       var method = methods[i];
       Test.Reporters.Composite.define(method, function(event) {
-        var fn;
-        for (var i = 0, n = this._reporters.length; i < n; i++) {
-          fn = this._reporters[i][method];
-          if (fn) fn.call(this._reporters[i], event);
-        }
+        this._queue[event.eventId] = [method, event];
+        this.flush();
       });
     })(n);
 })();
